699cefadda2d0d8e64fd30afd40baec1d37e7c9f
lahm89 <56438363+lahm89@users.noreply.github.com>
2020-05-08 20:43:34 +0900

    team commit

---
+++ src/helloworld/HelloWorld.java
@@ -0,0 +1,33 @@
+package helloworld;
+
+/* 복숭아 농장 영상 기대되욯ㅎ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+	
+		System.out.println("HelloWorld"); //컨트롤 에프11하고 같이 run 
+		System.out.println("run");
+		System.out.println("Hell"); //컨트롤 에프11하고 같이 run 
+		System.out.println("ui");
+		int num1 = 10; //10이 초기값 =는 대입연산자 
+		double num2 = 45.3;
+		float num10 = (float)4665.3234;
+		double num3 = 0.56;
+		int number = ten();
+		double sum = num2 + num3;
+		System.out.println(number);
+		String name$ = "변백현";
+		String Name = "rkskek";
+		System.out.println(num1);
+		System.out.println(num2);
+		System.out.println(num3);
+		System.out.println(name$);
+		//값(value)
+		System.out.println(num1 +":"+ num2);
+	}
+	
+	public static int ten() {
+		return 10;
+	}
+}
